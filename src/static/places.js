aff06e31ce5b1a2263840f2a929d957b9243c336
Mohammed Fahim Ahmed <faizu.offl@gmail.com>
2022-11-18 01:06:38 +0400

    initial commit with changes

---
+++ src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+  {
+    title: 'The Counter',
+    description:
+      "In digital logic and computing, a counter is a device which stores the number of times a particular event or process has occurred, often in relationship to a clock.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Bora Bora',
+    description:
+      'Shaped like a giant sombrero, this lush volcanic island stars in countless South Pacific fantasies. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+    time: 1500,
+  },
+];
+
+export default places;
